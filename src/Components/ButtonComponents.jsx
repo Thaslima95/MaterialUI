c3ca9cec24267f174a6f7fc9c1c07e0821fcbadc
Thaslima <nizuthasli15@gmail.com>
2023-08-24 17:56:10 +0530

    a24-2

--- src/Components/ButtonComponents.jsx
+++ src/Components/ButtonComponents.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import { Button } from "@mui/material";
 export default function ButtonComponents() {
-  return <div>ButtonComponents</div>;
+  return (
+    <div>
+      ButtonComponents
+      <Button variant="contained">Button Contained</Button>
+    </div>
+  );
 }
