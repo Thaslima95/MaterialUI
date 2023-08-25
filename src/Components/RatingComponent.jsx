ab83441080af6b3cda1bbb6bd37709e21747fef6
Thaslima <nizuthasli15@gmail.com>
2023-08-25 13:34:07 +0530

    rat

--- src/Components/RatingComponent.jsx
+++ src/Components/RatingComponent.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import Rating from "@mui/material/Rating";
 import Stack from "@mui/material/Stack";
+import { Typography } from "@mui/material";
+import { useState } from "react";
 
 export default function RatingComponent() {
+  const [value, setValue] = useState(1);
   return (
     <div>
       RatingComponent
-      <Stack spacing={1}>
+      <Stack spacing={3}>
         <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
         <Rating
           name="half-rating-read"
           defaultValue={2.5}
           precision={0.5}
           readOnly
+        />
+        <Typography component="legend">No rating given</Typography>
+        <Rating name="half-rating" value={null} />
+        <Typography component="legend">Controlled</Typography>
+        <Rating
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
         />
       </Stack>
     </div>
